94a18c8c415da5105b3558238eacf85c553e5de5
skysharvan <sharvan2829kumar@gmail.com>
2022-07-18 23:17:00 +0530

    ShoppingItems

---
+++ data.js
@@ -0,0 +1,56 @@
+let data={
+    "data": [
+        {
+            "name": "Cosmetics",
+            "productList": [
+                {
+                    "name": "Face wash",
+                    "price": 123
+                },
+                {
+                    "name": "Hair oil",
+                    "price": 12
+                }
+            ]
+        },
+        {
+            "name": "Household",
+            "productList": [
+                {
+                    "name": "Hair oil",
+                    "price": 122
+                },
+                {
+                    "name": "Face wash",
+                    "price": 123
+                }
+            ]
+        },
+        {
+            "name": "Household",
+            "productList": [
+                {
+                    "name": "Hair Oil",
+                    "price": 122
+                },
+                {
+                    "name": "Face wash",
+                    "price": 123
+                }
+            ]
+        },
+        {
+            "name": "Household",
+            "productList": [
+                {
+                    "name": "Hair Oil",
+                    "price": 122
+                },
+                {
+                    "name": "Face wash",
+                    "price": 123
+                }
+            ]
+        }
+    ]
+}
